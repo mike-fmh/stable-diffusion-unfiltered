376773f1510e8056b1646257c4ec70e3c6e36486
Michael Felix <65558188+mike-fmh@users.noreply.github.com>
2022-10-18 14:15:14 -0400

    Update mp4toimp.py

--- outputs/img2img-samples/mp4toimp.py
+++ outputs/img2img-samples/mp4toimp.py
@@ -7,10 +7,11 @@ length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 success,image = vidcap.read()
 count = 0
 storedir = "heatwave_imgs"
+
 if not os.path.exists(f"{storedir}"):
   os.makedirs(f"{storedir}")
 for count in tqdm.trange(length):
   image = cv2.resize(image, (512, 512))
-  cv2.imwrite(f"{storedir}/frame%d.png" % count, image)
+  cv2.imwrite(f"{storedir}/%d.png" % count, image)
   success,image = vidcap.read()
   #print('Read a new frame: ', success)
