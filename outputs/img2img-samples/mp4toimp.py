7d5a0855f6b2473e23697742d367ef66fb41610d
Michael Felix <65558188+mike-fmh@users.noreply.github.com>
2022-10-21 07:38:27 -0400

    Update mp4toimp.py

--- outputs/img2img-samples/mp4toimp.py
+++ outputs/img2img-samples/mp4toimp.py
@@ -6,12 +6,13 @@ vidcap = cv2.VideoCapture(r"C:\Users\mmh\Downloads\Y2Mate.is - Glass Animals - H
 length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 success,image = vidcap.read()
 count = 0
-storedir = "heatwave_imgs"
+width, height = 1024, 574
+storedir = f"heatwave_imgs_{width}x{height}"
 
 if not os.path.exists(f"{storedir}"):
   os.makedirs(f"{storedir}")
 for count in tqdm.trange(length):
-  image = cv2.resize(image, (512, 512))
+  image = cv2.resize(image, (width, height))
   cv2.imwrite(f"{storedir}/%d.png" % count, image)
   success,image = vidcap.read()
   #print('Read a new frame: ', success)
