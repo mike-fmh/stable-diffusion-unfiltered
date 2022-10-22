2fc4e9bf14e2def5d19bb3f4eaca8b886fd8c9ab
Michael Felix <65558188+mike-fmh@users.noreply.github.com>
2022-10-22 14:23:42 -0400

    Update mp4toimp.py

--- outputs/img2img-samples/mp4toimp.py
+++ outputs/img2img-samples/mp4toimp.py
@@ -2,12 +2,12 @@ import cv2
 import os
 import tqdm
 
-vidcap = cv2.VideoCapture(r"C:\Users\mmh\Downloads\Y2Mate.is - Glass Animals - Heat Waves (Official Video)-mRD0-GxqHVo-1080p-1656206039429.mp4")
+vidcap = cv2.VideoCapture(r"E:\Documents\shrek\videos_audios\shrek_allstar.mp4")
 length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 success,image = vidcap.read()
 count = 0
 width, height = 1024, 574
-storedir = f"heatwave_imgs_{width}x{height}"
+storedir = r"E:\Documents\shrek\frames\allstar"
 
 if not os.path.exists(f"{storedir}"):
   os.makedirs(f"{storedir}")
